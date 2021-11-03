217c31716b95140f8440b0daf8c9f190893c132e
de-jackson <jackyeyamu@gmail.com>
2021-11-03 16:15:47 +0300

    0. README, man, AUTHORS

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/* delete when built functions are called */
+#include <string.h>
+/* delete when built functions are called */
+
+
+
+#endif /* __SHELL_H__ */
